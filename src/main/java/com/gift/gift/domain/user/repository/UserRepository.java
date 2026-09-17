6a2555a5f216277af8ff68c25b74681378b3af8a
정연상 <jeong-yeonsang@jeong-yeonsangs-MacBook-Pro.local>
2026-09-17 11:32:19 +0900

    feat: User 레포지토리 추가

---
+++ src/main/java/com/gift/gift/domain/user/repository/UserRepository.java
@@ -0,0 +1,20 @@
+package com.gift.gift.domain.user.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.gift.gift.domain.user.entity.User;
+import com.gift.gift.domain.user.entity.UserStatus;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    boolean existsByEmail(String email);
+
+    Optional<User> findByEmail(String email);
+
+    Optional<User> findByIdAndStatusAndDeletedAtIsNull(
+            Long id,
+            UserStatus status
+    );
+}
