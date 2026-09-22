978d5d3b8ccf174e83e8b847c7c30aa3e4b7b32d
yeonsang-atom <jys1480_@naver.com>
2026-09-23 02:00:51 +0900

    refactor: 친구 도메인 예외 처리 구조 분리

---
+++ src/main/java/com/gift/gift/domain/friend/exception/FriendException.java
@@ -0,0 +1,10 @@
+package com.gift.gift.domain.friend.exception;
+
+import com.gift.gift.global.exception.BusinessException;
+
+public class FriendException extends BusinessException {
+
+    public FriendException(FriendErrorCode errorCode) {
+        super(errorCode.errorCode(), errorCode.message());
+    }
+}
