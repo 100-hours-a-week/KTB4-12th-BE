210bfef5e3c4248baab017cb4daaf1b231c3a262
yeonsang-atom <jys1480_@naver.com>
2026-09-24 09:14:10 +0900

    refactor: 사용하지 않은 import 제거

--- src/main/java/com/gift/gift/domain/user/controller/SignupPreflightController.java
+++ src/main/java/com/gift/gift/domain/user/controller/SignupPreflightController.java
@@ -1,12 +1,9 @@
 package com.gift.gift.domain.user.controller;
 
-import java.util.concurrent.ConcurrentHashMap;
-
 import jakarta.validation.Valid;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.util.MultiValueMap;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
